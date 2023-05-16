6992a68f80b2d4ccef14c6aa30b34ad4a8eeba04
mperkins1995 <mperkins1995@gmail.com>
2023-05-15 21:36:35 -0500

    added ship factory with getLength and hit functions using TDD

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,18 @@
-function Ship() {
-  const length = 4;
-  return { length };
+function Ship(length) {
+  const getLength = () => length;
+  const hit = () => {
+    length--;
+
+    if (length < 0) {
+      throw new Error("Ship is sunk");
+    }
+
+    return length;
+
+  };
+
+  return { getLength, hit };
+
 }
 
 module.exports = Ship;
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,6 +1,23 @@
-const Ship = require('./ship');
+const Ship = require("./ship");
 
-test('Ship has a length', () => {
+test("Ship has a length", () => {
   const ship = Ship(4);
-  expect(ship.length).toBe(4);
+  expect(ship.getLength()).toBe(4);
+});
+
+test("Ship has a hit function", () => {
+  const ship = Ship(4);
+  expect(ship.hit).toBeDefined();
+});
+
+test("Ship hit function reduces length", () => {
+  const ship = Ship(4);
+  ship.hit();
+  expect(ship.getLength()).toBe(3);
+});
+
+test("Ship hit function throws error when length is 0", () => {
+  const ship = Ship(1);
+  ship.hit();
+  expect(() => ship.hit()).toThrow("Ship is sunk");
 });
